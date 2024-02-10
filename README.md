eaf9fd788e02175a41381e6e439a47a036bd3f3f
Koen Betsens <koen@betsens.be>
2024-02-10 19:35:44 +0100

    Initial commit
    The Tailwind and Alpine based code

---
+++ README.md
@@ -0,0 +1,11 @@
+# fakescreen bankapp
+ A banking app simulation
+
+ ## Stack
+ App is hand-crafted with Alpine.js and Tailwind CSS.
+
+ ## Production
+ App is built for Putain.
+
+ ## Internal job sheet
+ https://docs.google.com/document/d/1f8C_9A54JityPeVYeidnHbXHAMCifKod
---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// const btn = document.getElementById('login');
+simulateLogin = function(btn){
+
+    const loginpage = document.getElementById('login-page');
+    const facepage = document.getElementById('face-page');
+    
+    loginpage.classList.add('animate-slideout')
+    facepage.classList.add('animate-slideout')
+}
